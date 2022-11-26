93a1cbe0c6df43cac8fa0010af687ba1b6b2b387
ScionRs <grazs116@gmail.com>
2022-11-26 16:28:58 +0300

    Modified Car.dart & created Equipment.dart

--- lib/model/Car.dart
+++ lib/model/Car.dart
@@ -1,9 +1,34 @@
 
 
-class Car{
-  int id;
-  String brand;
-  int price;
+import 'Equipment.dart';
 
-  Car({required this.id, required this.brand,required this.price});
+class Car{
+  final int id;
+  final String brand;
+  final String model;
+  final int price;
+  final String bodyColor;
+  // Год создания
+  final int productionYear;
+  // Тип кузова: Седан, Хэтчбек и тд
+  final String bodyType;
+  final String transmission;
+  //Тип двигателя: Бензиновый,Дизельный
+  final String engineType;
+  //Обьем двигателя
+  final double engineVolume;
+  final List<Equipment> equipment;
+
+  Car({
+     required this.id,
+    required this.brand,
+    required this.model,
+    required this.price,
+    required this.bodyColor,
+    required this.productionYear,
+    required this.bodyType,
+    required this.transmission,
+    required this.engineType,
+    required this.engineVolume,
+    required this.equipment });
 }
