513b4202a515048b79c1c91bb4119c3176118c8a
ScionRs <grazs116@gmail.com>
2022-11-28 20:13:48 +0300

    Added image field in car & car_category models

--- lib/model/car_category.dart
+++ lib/model/car_category.dart
@@ -4,9 +4,10 @@ import 'package:car_sale_app/model/Car.dart';
 class CarCategory{
   final String id;
   final String name;
+  final String image;
   final List<Car> cars;
 
-  CarCategory({required this.id,required this.name,required this.cars});
+  CarCategory({required this.id,required this.name,required this.image, required this.cars});
 
   factory CarCategory.fromJson(Map<String, dynamic> parsedJson){
     var carsListSize = parsedJson['size'] as List;
@@ -14,6 +15,7 @@ class CarCategory{
     return CarCategory(
         id: parsedJson['_id'],
         name: parsedJson['name'],
+        image: parsedJson['image'],
         cars: carJsonList
     );
   }
