7e037876dae1b7df3786ece797471f17350ed213
ScionRs <grazs116@gmail.com>
2022-11-30 17:08:47 +0300

    Added tabBar

--- lib/model/сommon_data.dart
+++ lib/model/сommon_data.dart
@@ -434,7 +434,7 @@ class CommonData{
         ])
   ];
 
-  var carCategoryList = [
+  static var carCategoryList = [
     CarCategory(
       id: '1',
       name: 'Mazda',
@@ -778,5 +778,56 @@ class CommonData{
               price: 3202964)
       )],
     ),
+  ];
+  static var carCategoryList2 = [
+  CarCategory(
+  id: '1',
+  name: 'Mazda',
+  image: 'https://www.tts.ru/upload/iblock/b57/mazda_logo.png',
+  cars: [Car(id: '1',
+  brand: 'Mazda',
+  model: 'Cx-5',
+  image: 'https://www.tts.ru/upload/iblock/91d/mazda-cx-5-vnedorozhnik-seryy-0.jpg',
+  price: 3202964,
+  bodyColor: 'Красный',
+  productionYear: 2022,
+  bodyType: 'Кроссовер',
+  transmission: 'АКПП',
+  engineType: 'Бензин',
+  engineVolume: 2.0,
+  equipment: Equipment(
+  id: '1',
+  title: 'Mazda CX-5 ACTIV 2.0AT 2WD KGB4914 <2019>',
+  seatUpholstery: 'Alcantara',
+  heatedSeats: 'Подогрев передних сидений',
+  airbags: '7 подушек безопасности',
+  onBoardComputer: 'Маршрутный компьютер',
+  horsePower: 149,
+  maxTorque: 208,
+  typeOfFuel: 'Бензин',
+  accelerationTime: 10.3,
+  maxSpeed: 200,
+  airConditioningSystem: '2-х зонный',
+  multifunctionSteeringWheel: 'Управление аудиосистемой на рулевом колесе',
+  fuelTankVolume: 58,
+  centralLocking: 'Центральный замок с дистанционным управлением',
+  trunkVolume: '442',
+  driveUnit: 'Передний',
+  additionalOptions: [
+  'Автоматическая блокировка дверных замков при начале движения',
+  'Электронный стояночный тормоз',
+  'Система Эра-Глонасс',
+  ]),
+  maintenanceCosts:
+  MaintenanceCosts(id: '1',
+  typeOfFuel: 'аи-95',
+  fuelConsumptionUrbanCycle: 8.9,
+  extraUrbanFuelConsumption: 6.2,
+  combinedFuelConsumption: 7.2,
+  travelDistance: 590,
+  horsePower: 149,
+  price: 3202964)
+  )],
+  ),
   ];
 }
