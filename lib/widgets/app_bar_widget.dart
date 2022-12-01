86b6e403e2447d60bfbc3dcb7108af812d91c5b2
nAdmin <vlladedellev@gmail.com>
2022-12-01 18:44:18 +0300

    Created AppBar in separated file №2

---
+++ lib/widgets/app_bar_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        title: const Text(
+          'CarMarket',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(0, 73, 183, 1));
+  }
+}
