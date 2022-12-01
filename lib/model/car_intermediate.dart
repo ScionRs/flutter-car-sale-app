f2c9317753f005e101b73d59adbb80e0f5500980
ScionRs <grazs116@gmail.com>
2022-12-01 19:03:07 +0300

    Added methods to car_intermediate

---
+++ lib/model/car_intermediate.dart
@@ -0,0 +1,39 @@
+
+import 'dart:math';
+
+import 'Car.dart';
+
+class CarIntermediate{
+  String id;
+  List<Car> carList;
+
+  CarIntermediate({required this.id,required this.carList});
+
+  factory CarIntermediate.fromJson(Map<String, dynamic> parsedJson){
+    var carsListSize = parsedJson['carList'] as List;
+    List<Car> carJsonList = carsListSize.map((e) => Car.fromJson(e)).toList();
+    return CarIntermediate(
+        id: parsedJson['_id'],
+        carList: carJsonList
+    );
+  }
+
+  String giveCarImage(){
+    return carList[0].image;
+  }
+  
+  int giveMaxPriceFromCar(){
+    var value = carList.first;
+    carList.forEach((element) {
+     if(element.price > value.price){
+       value = element;
+     }
+    });
+    return value.price;
+  }
+
+  int giveExpMaxPriceFromCar() => carList.map((e) => e.price).reduce(max);
+
+  int giveMinPriceFromCar() => carList.map((e) => e.price).reduce(min);
+
+}
