58138480c7a12780abd2ea1866283bb6e09ceadd
ScionRs <grazs116@gmail.com>
2022-12-07 17:45:36 +0300

    Added title & image & price in car_inter_screen

--- lib/car_intermediate_screen_widget/car_intermediate_screen_widget.dart
+++ lib/car_intermediate_screen_widget/car_intermediate_screen_widget.dart
@@ -1,5 +1,6 @@
 
 import 'package:car_sale_app/model/car_intermediate.dart';
+import 'package:car_sale_app/widgets/build_image.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,10 +10,40 @@ class CarIntermediateWidget extends StatelessWidget {
 
   CarIntermediateWidget({Key? key, required this.carIntermediate}) : super(key: key);
 
+  var textStyle = const TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('${carIntermediate.carList[0].brand} ',style: textStyle,),
+                  Text(carIntermediate.carList[0].model,style: textStyle,),
+                ],
+              ),
+            ),
+            BuildImage(url: carIntermediate.carList[0].image),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('${carIntermediate.giveMinPriceFromCar()} ',style: textStyle,),
+                Text('-'),
+                Text('${carIntermediate.giveExpMaxPriceFromCar()}',style: textStyle),
+              ],
+            )
+
+          ],
+        ),
+      )
     );
   }
 }
