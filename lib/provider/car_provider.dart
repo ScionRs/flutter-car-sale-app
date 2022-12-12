d2052eab4c65e198e908fc3ceb562cfdcc18bf59
ScionRs <grazs116@gmail.com>
2022-12-12 12:25:03 +0300

    Refactor car_intermediate model

--- lib/provider/car_provider.dart
+++ lib/provider/car_provider.dart
@@ -3,11 +3,6 @@ import 'package:car_sale_app/model/%D1%81ommon_data.dart';
 import 'package:car_sale_app/model/Car.dart';
 import 'package:car_sale_app/model/Dealer.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../model/City.dart';
-import '../model/Equipment.dart';
-import '../model/maintenance_costs.dart';
 
 class CarProvider with ChangeNotifier{
 
