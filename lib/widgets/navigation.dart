18990601986ca32dacf6ba80c2c2f6a95e8d8cfa
nAdmin <vlladedellev@gmail.com>
2023-01-30 18:55:40 +0300

    Created redesigned_main_screen

--- lib/widgets/navigation.dart
+++ lib/widgets/navigation.dart
@@ -5,6 +5,7 @@ import 'package:car_sale_app/main_screen_widget/main_screen_widget.dart';
 import 'package:car_sale_app/model/City.dart';
 import 'package:car_sale_app/model/car_category.dart';
 import 'package:car_sale_app/model/car_intermediate.dart';
+import 'package:car_sale_app/redesigned_main_screen/redesigned_main_screen.dart';
 import 'package:flutter/material.dart';
 
 abstract class MainNavigationRouteName {
