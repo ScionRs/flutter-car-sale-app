1d7b330711df726ed00d966949e503903f874b45
ScionRs <grazs116@gmail.com>
2023-01-31 14:46:28 +0300

    Added AppImages class and Updates pubspec

---
+++ lib/app_values/app_images.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class AppImages{
+  static const mazda_cx_5_white = AssetImage('images/img_car/mazda-cx-5-vnedorozhnik-belyy-10.png');
+  static const mazda_cx_5_pink = AssetImage('images/img_car/mazda-cx-5-vnedorozhnik-krasnyy-bordovyy-rozovyy-0.png');
+  static const mazda_cx_5_black = AssetImage('images/img_car/mazda-cx-5-vnedorozhnik-chernyy-0.png');
+  static const mazda_cx_5_grey = AssetImage('images/img_car/mazda-cx-5-vnedorozhnik-seryy-0.png');
+  static const mazda_cx_5_darkblue = AssetImage('images/img_car/mazda-cx-5-vnedorozhnik-siniy-goluboy-fioletovyy-0.png');
+}
