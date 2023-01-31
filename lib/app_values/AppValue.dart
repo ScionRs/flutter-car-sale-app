41766d94698694346a9bb655ad357e9bd5ff8cd3
ScionRs <grazs116@gmail.com>
2023-01-31 15:32:43 +0300

    added value customBackgroundGreyCard2 in AppValue

--- lib/app_values/AppValue.dart
+++ lib/app_values/AppValue.dart
@@ -26,4 +26,5 @@ class AppValue {
   static final customTextStyleBlack = TextStyle(color: Colors.black);
   static final customBackgroundWhite = Color.fromRGBO(255, 255, 255, 1);
   static final customBackgroundGreyCard = Color.fromRGBO(229, 231, 235, 1);
+  static final customBackgroundGreyCard2 = Color.fromRGBO(252, 252, 253, 1);
 }
