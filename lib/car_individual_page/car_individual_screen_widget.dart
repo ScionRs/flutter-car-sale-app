664def28b1409045a98ae431f76b1fc38b49ba85
ScionRs <grazs116@gmail.com>
2023-02-01 14:29:15 +0300

    Added _ExpansionTileWidget

--- lib/car_individual_page/car_individual_screen_widget.dart
+++ lib/car_individual_page/car_individual_screen_widget.dart
@@ -18,9 +18,80 @@ class CarIndividualWidget extends StatefulWidget {
 }
 
 class _CarIndividualState extends State<CarIndividualWidget> {
+
+
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
+    // Список опций автомобиля по умолчанию
+    var tableListRowEquipment = [
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Название комплектации:", car: widget.car.equipment.title.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Привод:", car: widget.car.equipment.driveUnit.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Мощность(лс):", car: widget.car.equipment.horsePower.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Время разгона:", car: widget.car.equipment.accelerationTime.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Обороты:", car: widget.car.equipment.maxTorque.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Типо топлива:", car: widget.car.equipment.typeOfFuel.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Подушки безопасности:", car: widget.car.equipment.airbags.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Обьем бензобака:", car: widget.car.equipment.fuelTankVolume.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Обьем багажника:", car: widget.car.equipment.trunkVolume.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Климат контроль:", car: widget.car.equipment.airConditioningSystem.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Бортовой компьютер:", car: widget.car.equipment.onBoardComputer.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Центральный замок:", car: widget.car.equipment.centralLocking.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Подогрев сидений:", car: widget.car.equipment.heatedSeats.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Максимальная скорость:", car: widget.car.equipment.maxSpeed.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Мультируль:", car: widget.car.equipment.multifunctionSteeringWheel.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Обивка сидений:", car: widget.car.equipment.seatUpholstery.toString(), textTheme: textTheme)
+      ]),
+    ];
+    var tableListRowDefaultCarOptions = [
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Год выпуска:", car: widget.car.productionYear.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Тип кузова:", car: widget.car.bodyType.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Трансмиссия:", car: widget.car.transmission.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Тип двигателя:", car: widget.car.engineType.toString(), textTheme: textTheme)
+      ]),
+      TableRow(children: [
+        _TableRowCustomWidget(description: "Обьем двигателя", car: widget.car.engineVolume.toString(), textTheme: textTheme)
+      ]),
+    ];
+
     return Scaffold(
       backgroundColor: AppColors.white,
       appBar: AppBar(
@@ -47,12 +118,100 @@ class _CarIndividualState extends State<CarIndividualWidget> {
               ),
             ),
           ),
-
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: _ExpansionTileWidget(name:"Основные характеристики", tableListRowDefaultCarOptions: tableListRowDefaultCarOptions),
+          ),
+          _ExpansionTileWidget(name:"Комплектация", tableListRowDefaultCarOptions: tableListRowEquipment),
         ],
       )
     );
   }
 }
+
+class _TableRowCustomWidget extends StatelessWidget {
+  final String description;
+  final String car;
+  final TextTheme textTheme;
+
+  const _TableRowCustomWidget({
+    Key? key,
+    required this.description,
+    required this.car,
+    required this.textTheme,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical:  10.0),
+          child: Text('${description}',  style: textTheme.titleLarge),
+        ),
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical:  10.0),
+            child: Text('${car.toString()}',maxLines: 2, style: textTheme.titleLarge, textAlign: TextAlign.right, overflow: TextOverflow.ellipsis,),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// Раскрывающийся список
+class _ExpansionTileWidget extends StatelessWidget {
+  const _ExpansionTileWidget({
+    Key? key,
+    required this.name,
+    required this.tableListRowDefaultCarOptions,
+  }) : super(key: key);
+  final String name;
+  final List<TableRow> tableListRowDefaultCarOptions;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text('${name}', textAlign: TextAlign.center, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+      collapsedBackgroundColor: Color.fromRGBO(0, 73, 183, 1),
+      collapsedTextColor: Colors.white,
+      textColor: Color.fromRGBO(0, 73, 183, 1),
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Container(
+            padding: const EdgeInsets.all(6.0),
+              child: _TableWidget(tableRowList: tableListRowDefaultCarOptions,)),
+        ),
+      ],
+    );
+  }
+}
+
+// Таблица для опций
+class _TableWidget extends StatelessWidget {
+  final List<TableRow> tableRowList;
+  const _TableWidget({
+    Key? key,
+    required this.tableRowList,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+        border:TableBorder(
+            horizontalInside: BorderSide(
+                width: 1,
+                color: AppColors.grey,
+                style: BorderStyle.solid)),
+      children: tableRowList
+    );
+  }
+}
+
 
 // Колонка с характеристикой
 class _ColumnWidget extends StatelessWidget{
