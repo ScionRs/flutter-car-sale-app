49ab7243503a1a99cc8b71ac0c877ce40139a30f
ScionRs <grazs116@gmail.com>
2023-02-01 15:15:18 +0300

    Added width and height to BuildLocalImage class

--- lib/widgets/build_local_image.dart
+++ lib/widgets/build_local_image.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 
 class BuildLocalImage extends StatelessWidget {
   String url;
-  BuildLocalImage({Key? key, required this.url}) : super(key: key);
+  double? width;
+  double? height;
+  BuildLocalImage({Key? key, required this.url, this.width, this.height}) : super(key: key);
 
   Widget buildImage(String urlImage) => Container(
       margin: const EdgeInsets.symmetric(horizontal: 0),
       color: Colors.transparent,
       child: Image.asset(
         urlImage,
+        width: width,
+        height: height,
         fit: BoxFit.cover,
       ));
 
