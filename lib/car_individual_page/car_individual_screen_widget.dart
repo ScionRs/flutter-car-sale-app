5c86e147ba0842619f716e8020ef1959739b0a65
ScionRs <grazs116@gmail.com>
2023-02-01 11:02:35 +0300

    Added image & color gradients in car_individual_widget

--- lib/car_individual_page/car_individual_screen_widget.dart
+++ lib/car_individual_page/car_individual_screen_widget.dart
@@ -2,6 +2,7 @@
 import 'package:car_sale_app/app_values/AppValue.dart';
 import 'package:car_sale_app/model/Car.dart';
 import 'package:car_sale_app/theme/constants.dart';
+import 'package:car_sale_app/widgets/build_local_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -21,20 +22,55 @@ class _CarIndividualState extends State<CarIndividualWidget> {
     TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
       backgroundColor: AppColors.white,
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: _TitleWidget(car: widget.car),
+        centerTitle: true,
+      ),
       body: ListView(
         shrinkWrap: true,
         physics: const BouncingScrollPhysics(),
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         children: [
-          _TitleWidget(car: widget.car,),
-          
+          _ImageContainerWidget(car: widget.car),
         ],
       )
     );
   }
 }
 
+// Изображение и градиентный фон
+class _ImageContainerWidget extends StatelessWidget {
+  final Car car;
+  const _ImageContainerWidget({
+    Key? key,
+    required this.car,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [
+            0.1,
+            0.6,
+            0.9,
+          ],
+          colors: [
+            Color.fromRGBO(15, 23, 42, 1),
+            Color.fromRGBO(75, 85, 99, 1),
+            Color.fromRGBO(15, 23, 42, 1)
+          ],
+        )
+    ),
+    child: BuildLocalImage(url:car.image)
+    );
+  }
+}
+
+// Название бренда и модели
 class _TitleWidget extends StatelessWidget {
   final Car car;
   const _TitleWidget({
