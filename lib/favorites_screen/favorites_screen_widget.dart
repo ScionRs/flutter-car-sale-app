8d9fc977a1b2d30905193d3f3fb0c300427eed97
nAdmin <vlladedellev@gmail.com>
2023-02-03 12:20:14 +0300

    Created favorites_screen

---
+++ lib/favorites_screen/favorites_screen_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavoritesScreenWidget extends StatefulWidget {
+  const FavoritesScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FavoritesScreenWidget> createState() => _FavoritesScreenWidgetState();
+}
+
+class _FavoritesScreenWidgetState extends State<FavoritesScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
