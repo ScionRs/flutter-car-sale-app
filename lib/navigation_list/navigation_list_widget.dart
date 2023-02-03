75d23bb3268d5c4f16eea3ec9ef20bb80dd3203a
nAdmin <vlladedellev@gmail.com>
2023-02-03 18:29:33 +0300

    Created navigation_list

---
+++ lib/navigation_list/navigation_list_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../bottom_navigation_bar/bottom_navigation_bar.dart';
+import '../favorites_screen/favorites_screen_widget.dart';
+import '../redesigned_main_screen/redesigned_main_screen.dart';
+
+class NavigationListWidget extends StatefulWidget {
+  const NavigationListWidget({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationListWidget> createState() => _NavigationListWidgetState();
+}
+
+class _NavigationListWidgetState extends State<NavigationListWidget> {
+  int _selectedTab = 1;
+
+  void onSelectedTab(int index) {
+    if (_selectedTab == index) return;
+    setState(
+      () {
+        _selectedTab = index;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        children: [
+          RedesignedMainScreenWidget(),
+          FavoritesScreenWidget(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBarWidget(),
+    );
+  }
+}
