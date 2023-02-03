8ce315c59f73cb280fc1e3f8cd01b99920102deb
nAdmin <vlladedellev@gmail.com>
2023-02-03 12:26:12 +0300

    Added AppBar in FavoritesScreenWidget

--- lib/favorites_screen/favorites_screen_widget.dart
+++ lib/favorites_screen/favorites_screen_widget.dart
@@ -10,6 +10,10 @@ class FavoritesScreenWidget extends StatefulWidget {
 class _FavoritesScreenWidgetState extends State<FavoritesScreenWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Желаемое'),
+      ),
+    );
   }
 }
