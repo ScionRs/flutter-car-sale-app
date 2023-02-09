60f94f27e571274f1c5bd7a448f33dcd4d2acb07
ScionRs <grazs116@gmail.com>
2023-02-09 12:12:42 +0300

    Created IndividualCarCard

--- lib/widgets/car_card_widget.dart
+++ lib/widgets/car_card_widget.dart
@@ -165,3 +165,111 @@ class _CarCardWidgetState extends State<CarCardWidget> {
     );
   }
 }
+
+class IndividualCarCardWidget extends StatefulWidget {
+  final Key key;
+  final Car car;
+  final ValueChanged<bool> isSelected;
+  IndividualCarCardWidget({required this.key, required this.car, required this.isSelected});
+
+  @override
+  _IndividualCarCardWidgetState createState() => _IndividualCarCardWidgetState();
+}
+
+class _IndividualCarCardWidgetState extends State<IndividualCarCardWidget> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        decoration: const BoxDecoration(
+            color: AppColors.customBackgroundGreyCard,
+            borderRadius: BorderRadius.all(Radius.circular(20))
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+                children: [
+                  BuildLocalImage(url: widget.car.image),
+                  Positioned(
+                    right: 10,
+                    top: 10,
+                    child: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          isSelected = !isSelected;
+                          widget.isSelected(isSelected);
+                        });
+                      },
+                      icon: const Icon(Icons.favorite, size: 35.0,),
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      color: isSelected ? Colors.red : Colors.grey,
+                    ),
+                  )
+                ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(widget.car.equipment.title,
+                textAlign: TextAlign.left,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Divider(
+                  color: Colors.grey
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(top: 5.0),
+                        child: Icon(MyFlutterApp.engine, size: 30.0,),
+                      ),
+                      Text('${widget.car.equipment.horsePower} лс',
+                        style: const TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold
+                        ),),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: Row(
+                      children: [
+                        const Icon(MyFlutterApp.gearshift, size: 30.0,),
+                        Text('${widget.car.transmission}',
+                          style: const TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold
+                          ),),
+                      ],
+                    ),
+                  ),
+                  //Text('${formatPrice.format(carItem.price).replaceAll(',', ' ')} ₽ ',style: textStylePrice,),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
