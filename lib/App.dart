648289a5b0020f9212f1ede7e1b30a141e18bbc1
ScionRs <grazs116@gmail.com>
2023-02-27 11:34:05 +0300

    debugShowCheckedModeBanner disabled

--- lib/App.dart
+++ lib/App.dart
@@ -10,6 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: setTheme(),
       routes: mainNavigation.routes,
       initialRoute: mainNavigation.initialRoute,
